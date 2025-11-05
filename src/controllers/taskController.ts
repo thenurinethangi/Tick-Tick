3e09090ed464dbf498c641cf2cbaaa82955bd628
thenurinethangi <thenurinathangi@gmail.com>
2025-11-05 19:08:51 +0530

    develop edit priority

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -132,3 +132,51 @@ export const editDate = async (req: AuthRequest,res: Response) => {
         return;
     }
 }
+
+
+export const editPriority = async (req: AuthRequest,res: Response) => {
+
+    const {id,priority} = req.body;
+
+    if(!id || !priority){
+        res.status(400).json({message: 'Need Task ID And Priority To Edit The Task Priority!', data: null});
+        return;
+    }
+
+    let filter1 = {_id: id};
+    const task = await Task.findOne(filter1);
+    if(!task){
+        res.status(404).json({message: 'Task Not Found!', data: null});
+        return;
+    }
+
+    if(!req.username){
+        res.status(401).json({message: 'Please Signin!', data: null});
+        return;
+    }
+
+    let filter2 = {username: req.username};
+    const user = await User.findOne(filter2);
+    if(!user){
+        res.status(401).json({message: 'Please Signup!', data: null});
+        return;
+    }
+
+    if(task.userId.toString() !== user._id.toString()){
+        res.status(400).json({message: 'You Can Only Edit Your Tasks!', data: null});
+        return;
+    }
+
+    let filter3  = {_id: task._id};
+    let updateQuery = {priority: priority};
+    const result = await Task.updateOne(filter3,updateQuery);
+
+    if(result){
+        res.status(202).json({message: 'Successfully Edited Priority Of Task ID: '+task._id, data: null});
+        return;
+    }
+    else{
+        res.status(500).json({message: 'Failed To Edit Priority Of The Task, Try Later!', data: null});
+        return;
+    }
+}
--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addTask, deleteTask, editDate } from '../controllers/taskController';
+import { addTask, deleteTask, editDate, editPriority } from '../controllers/taskController';
 import { authentication } from '../middlewares/authentication';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/add',authentication,addTask);
 router.delete('/delete/:id',authentication,deleteTask);
 router.put('/edit/date',authentication,editDate);
+router.put('/edit/priority',authentication,editPriority);
 
 export default router;
