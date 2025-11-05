b0b7dd38badc14a5da958ab47b0ab46d70d2a455
thenurinethangi <thenurinathangi@gmail.com>
2025-11-05 18:36:33 +0530

    develop task delete

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -37,3 +37,49 @@ export const addTask = async (req: AuthRequest,res: Response) => {
     res.status(202).json({message: 'Successfully Added New Task!', data: savedTask});
     return;
 }
+
+export const deleteTask = async (req: AuthRequest,res: Response) => {
+
+    const _id = req.params.id;
+
+    if(!_id){
+        res.status(400).json({message: 'Need Task ID To Delete The Task!', data: null});
+        return;
+    }
+
+    let filter1 = {_id: _id};
+    const task = await Task.findOne(filter1);
+    if(!task){
+        res.status(404).json({message: 'Task Not Found!', data: null});
+        return;
+    }
+
+    if(!req.username){
+        res.status(401).json({message: 'Please Signin!', data: null});
+        return;
+    }
+
+    let filter2 = {username: req.username};
+    const user = await User.findOne(filter2);
+    if(!user){
+        res.status(401).json({message: 'Please Signup!', data: null});
+        return;
+    }
+
+    if(task.userId.toString() !== user._id.toString()){
+        res.status(400).json({message: 'You Can Only Delete Your Tasks!', data: null});
+        return;
+    }
+
+    let filter3  = {_id: task._id};
+    const result = await Task.deleteOne(filter3);
+
+    if(result){
+        res.status(202).json({message: 'Successfully Deleted Task ID: '+task._id, data: null});
+        return;
+    }
+    else{
+        res.status(5000).json({message: 'Failed To Delete The Task, Try Later!', data: null});
+        return;
+    }
+}
--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addTask } from '../controllers/taskController';
+import { addTask, deleteTask } from '../controllers/taskController';
 import { authentication } from '../middlewares/authentication';
 
 const router = express.Router();
 
 router.post('/add',authentication,addTask);
+router.delete('/delete/:id',authentication,deleteTask);
 
 export default router;
